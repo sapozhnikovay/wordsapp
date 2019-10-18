10fd7bcae3430141ce554134b8f160e884c09f68
Alexey Sapozhnikov <a.sapozhnikov@javad.com>
2019-10-18 17:37:05 +0300

    More linting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
     apiKey: 'AIzaSyC1WcXDscBovJep0QkXxYuU6ac0fm5GEuo',
     authDomain: 'words-app-ce6db.firebaseapp.com',
     databaseURL: 'https://words-app-ce6db.firebaseio.com',
-    projectId: 'words-app-ce6db',
+    projectId: 'words-app-ce6db'
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
     apiKey: 'AIzaSyC1WcXDscBovJep0QkXxYuU6ac0fm5GEuo',
     authDomain: 'words-app-ce6db.firebaseapp.com',
     databaseURL: 'https://words-app-ce6db.firebaseio.com',
-    projectId: 'words-app-ce6db',
+    projectId: 'words-app-ce6db'
   }
 };
 
